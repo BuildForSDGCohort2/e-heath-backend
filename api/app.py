e1a7a11377dddb6ba7b97b614f69c8bc638fee01
kay-ogunbo <o.ogunbowal@gmail.com>
2020-09-04 11:13:42 +0100

    Setup the postgres database and the user model

--- api/app.py
+++ api/app.py
@@ -1,8 +1,42 @@
 from flask import Flask
+from flask_migrate import Migrate
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return {'Hello': 'World'}
+from api.extentions import db
+from api.extentions import mail
+from api.extentions import login_manager
 
+
+def create_app(settings_override=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    migrate = Migrate(app, db)
+
+    app.config.from_object("config.settings")
+    app.config.from_pyfile("settings.py", silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    
+    # with app.app_context():
+    #     from cli import commands
+
+    extensions(app)
+
+
+    return app
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+    mail.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    return None
