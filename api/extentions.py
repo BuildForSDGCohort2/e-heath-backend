bd70917870ae2f74acd765ecdb97909808662dd2
kay-ogunbo <o.ogunbowal@gmail.com>
2020-09-08 23:02:23 +0100

    Implemented login module

--- api/extentions.py
+++ api/extentions.py
@@ -1,8 +1,12 @@
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 
 mail = Mail()
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
+
